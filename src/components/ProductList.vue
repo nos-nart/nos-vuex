cde2508a2f4caae3a5acd15cd79d180a1291a6ba
nos-nart <nosnart1510@gmail.com>
2020-12-02 23:50:22 +0700

    [+] hh

---
+++ src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul v-for="product in products" :key="product.id">
+      <li>{{ product.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import shop from '@/api/shop';
+
+export default {
+  data() {
+    return {
+      products: []
+    }
+  },
+  created() {
+    shop.getProducts((products) => {
+      this.products = products;
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
