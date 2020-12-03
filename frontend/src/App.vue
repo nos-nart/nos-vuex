79e7a215e7bd105d7ca21efc8a4f799790ee8c9d
nam hoang <nam.hoang@ntq-solution.com.vn>
2020-12-03 15:52:59 +0700

    [+] xx

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <TodoPage />
+  </div>
+</template>
+
+<script>
+import TodoPage from '@/components/TodoPage.vue'
+export default {
+  name: 'App',
+  components: {
+    TodoPage
+  }
+}
+</script>
+
+<style>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "./plugins/element-ui.js";
+import store from './vuex/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
