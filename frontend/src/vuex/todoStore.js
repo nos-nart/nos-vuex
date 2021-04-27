4ee0e0e6d62ec977f0dc1f50681dd178f360095a
nos-nart <nosnart1510@gmail.com>
2021-04-27 23:55:29 +0700

    [~] 🧩 ff

--- frontend/src/vuex/todoStore.js
+++ frontend/src/vuex/todoStore.js
@@ -1,91 +1,105 @@
 import todoService from "./todoService";
 
-export default {
-  namespaced: true,
+const LIST_STARTED = 'LIST_STARTED';
+const LIST_SUCCESS = 'LIST_SUCCESS';
+const LIST_ERROR = 'LIST_ERROR';
+const CREATE_STARTED = 'CREATE_STARTED';
+const CREATE_SUCCESS = 'CREATE_SUCCESS';
+const CREATE_ERROR = 'CREATE_ERROR';
+const DESTROY_STARTED = 'DESTROY_STARTED';
+const DESTROY_SUCCESS = 'DESTROY_SUCCESS';
+const DESTROY_ERROR = 'DESTROY_ERROR';
 
-  state: {
-    loading: false,
-    list: []
+const state = {
+  loading: false,
+  list: []
+};
+
+const getters = {
+  list: state => state.list || [],
+  loading: state => !!state.loading
+}
+
+const mutations = {
+  [LIST_STARTED](state) {
+    state.loading = true;
   },
 
-  getters: {
-    list: state => state.list || [],
-    loading: state => !!state.loading
+  [LIST_SUCCESS](state, payload) {
+    state.list = payload;
+    state.loading = false;
   },
 
-  mutations: {
-    LIST_STARTED(state) {
-      state.loading = true;
-    },
+  [LIST_ERROR](state, payload) {
+    console.error(payload);
+    state.loading = false;
+  },
 
-    LIST_SUCCESS(state, payload) {
-      state.list = payload;
-      state.loading = false;
-    },
+  [CREATE_STARTED](state) {
+    state.loading = true;
+  },
 
-    LIST_ERROR(state, payload) {
-      console.error(payload);
-      state.loading = false;
-    },
+  [CREATE_SUCCESS](state) {
+    state.loading = false;
+  },
 
-    CREATE_STARTED(state) {
-      state.loading = true;
-    },
+  [CREATE_ERROR](state, payload) {
+    console.error(payload);
+    state.loading = false;
+  },
 
-    CREATE_SUCCESS(state) {
-      state.loading = false;
-    },
+  [DESTROY_STARTED](state) {
+    state.loading = true;
+  },
 
-    CREATE_ERROR(state, payload) {
-      console.error(payload);
-      state.loading = false;
-    },
+  [DESTROY_SUCCESS](state) {
+    state.loading = false;
+  },
 
-    DESTROY_STARTED(state) {
-      state.loading = true;
-    },
+  [DESTROY_ERROR](state, payload) {
+    console.error(payload);
+    state.loading = false;
+  }
+}
 
-    DESTROY_SUCCESS(state) {
-      state.loading = false;
-    },
-
-    DESTROY_ERROR(state, payload) {
-      console.error(payload);
-      state.loading = false;
+const actions = {
+  async doList({ commit }) {
+    try {
+      commit(LIST_STARTED);
+      const list = await todoService.list();
+      commit(LIST_SUCCESS, list);
+    } catch (error) {
+      commit(LIST_ERROR, error);
     }
   },
 
-  actions: {
-    async doList({ commit }) {
-      try {
-        commit("LIST_STARTED");
-        const list = await todoService.list();
-        commit("LIST_SUCCESS", list);
-      } catch (error) {
-        commit("LIST_ERROR", error);
-      }
-    },
+  async doCreate({ commit, dispatch }, payload) {
+    try {
+      commit(CREATE_STARTED);
+      await todoService.create(payload);
+      await dispatch("doList");
+      commit(CREATE_SUCCESS);
+    } catch (error) {
+      commit(CREATE_ERROR, error);
+    }
+  },
 
-    async doCreate({ commit, dispatch }, payload) {
-      try {
-        commit("CREATE_STARTED");
-        await todoService.create(payload);
-        await dispatch("doList");
-        commit("CREATE_SUCCESS");
-      } catch (error) {
-        commit("CREATE_ERROR", error);
-      }
-    },
-
-    async doDestroy({ commit, dispatch }, payload) {
-      try {
-        commit("DESTROY_STARTED");
-        await todoService.destroy(payload);
-        await dispatch("doList");
-        commit("DESTROY_SUCCESS");
-      } catch (error) {
-        commit("DESTROY_ERROR", error);
-      }
+  async doDestroy({ commit, dispatch }, payload) {
+    try {
+      commit(DESTROY_STARTED);
+      await todoService.destroy(payload);
+      await dispatch("doList");
+      commit(DESTROY_SUCCESS);
+    } catch (error) {
+      commit(DESTROY_ERROR, error);
     }
   }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
 };
